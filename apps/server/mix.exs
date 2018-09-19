cda6c1f4865a5744e8598fcc2549f7b0da6b69b7
Merisa Furchtgott <merisa.furchtgott@gmail.com>
2018-09-19 07:12:45 -0700

    Some minor config tweaks.

---
+++ apps/server/mix.exs
@@ -0,0 +1,34 @@
+defmodule Server.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :server,
+      version: "0.1.0",
+      build_path: "../../_build",
+      config_path: "../../config/config.exs",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
+      elixir: "~> 1.7",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {Server.Application, []}
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:cowboy, "~> 2.4.0"},
+      {:ranch, "~>1.6.0"},
+      {:power_assert, "~>0.1.2", only: :test}
+    ]
+  end
+end
